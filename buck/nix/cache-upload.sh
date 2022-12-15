6a708579b6676de0c90f78f50e1a70cad47e1cc2
Austin Seipp <aseipp@pobox.com>
2022-12-15 17:13:59 -0600

    github/buck: cache upload tweaks

--- buck/nix/cache-upload.sh
+++ buck/nix/cache-upload.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 set -e; set -f; export IFS=' '
-[[ -z "$OUT_PATHS" ]] && echo "no out paths provided" && exit
+[[ -z "$OUT_PATHS" ]] && echo "no out paths provided" && exit 1
 
 tsbin="/nix/var/nix/profiles/default/bin/ts"
 user=${USER:-root}
 [[ ! -f "$tsbin" ]] && tsbin="/nix/var/nix/profiles/per-user/$user/profile/bin/ts"
-[[ ! -f "$tsbin" ]] && echo "no task spooler installed! exiting" && exit 1
+[[ ! -f "$tsbin" ]] && echo "no task spooler installed! exiting" && exit 2
 
 S3_BUCKET="${S3_BUCKET:-__S3_BUCKET__}"
 S3_ENDPOINT="${S3_ENDPOINT:-__S3_ENDPOINT__}"
 export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:-__AWS_ACCESS_KEY_ID__}"
 export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:-__AWS_SECRET_ACCESS_KEY__}"
 
-[[ $S3_BUCKET == "__S3_BUCKET__" ]] && echo "S3_BUCKET not set, exiting" && exit 1
-[[ $S3_ENDPOINT == "__S3_ENDPOINT__" ]] && echo "S3_ENDPOINT not set, exiting" && exit 1
-[[ $AWS_ACCESS_KEY_ID == "__AWS_ACCESS_KEY_ID__" ]] && echo "AWS_ACCESS_KEY_ID not set, exiting" && exit 1
-[[ $AWS_SECRET_ACCESS_KEY == "__AWS_SECRET_ACCESS_KEY__" ]] && echo "AWS_SECRET_ACCESS_KEY not set, exiting" && exit 1
+[[ $S3_BUCKET == "__S3_BUCKET__" ]] && echo "S3_BUCKET not set, exiting" && exit 3
+[[ $S3_ENDPOINT == "__S3_ENDPOINT__" ]] && echo "S3_ENDPOINT not set, exiting" && exit 4
+[[ $AWS_ACCESS_KEY_ID == "__AWS_ACCESS_KEY_ID__" ]] && echo "AWS_ACCESS_KEY_ID not set, exiting" && exit 5
+[[ $AWS_SECRET_ACCESS_KEY == "__AWS_SECRET_ACCESS_KEY__" ]] && echo "AWS_SECRET_ACCESS_KEY not set, exiting" && exit 6
 
 echo "Uploading paths" $DRV_PATH " " $OUT_PATHS
   $tsbin /nix/var/nix/profiles/default/bin/nix copy \
