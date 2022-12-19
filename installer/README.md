800a4572b9ba644da917fb944a09a53b8f171dd7
Austin Seipp <aseipp@pobox.com>
2022-12-19 14:14:55 -0600

    feat(installer): initial commit

---
+++ installer/README.md
@@ -0,0 +1,7 @@
+## installer
+
+It is intended for this tool can bootstrap the repository for development.
+
+```bash
+nix run --accept-flake-config github:thoughtpolice/buck2-nix?dir=installer
+```
---
+++ installer/flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "XXX FIXME (aseipp): test installer";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  nixConfig = {
+    extra-substituters = "https://buck2-nix-cache.aseipp.dev/";
+    trusted-public-keys = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= buck2-nix-preview.aseipp.dev-1:sLpXPuuXpJdk7io25Dr5LrE9CIY1TgGQTPC79gkFj+o=";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    let myOverlay = (final: _: {
+      installer = final.callPackage (
+        { stdenv
+        , rustPlatform
+        }:
+
+        stdenv.mkDerivation rec {
+          pname = "installer";
+          version = "0.1.0";
+
+          unpackPhase = ":";
+          installPhase = ''
+            mkdir -p $out/bin
+            echo 'echo hello' > $out/bin/installer
+            chmod +x $out/bin/installer
+          '';
+        }) { };
+    });
+    in flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ (import rust-overlay) myOverlay ];
+      }; in rec {
+        packages.installer = pkgs.installer;
+        apps.installer = flake-utils.lib.mkApp { drv = pkgs.installer; };
+
+        defaultPackage = packages.installer;
+        defaultApp = apps.installer;
+      }
+    );
+}
