13b2c64909105bf53b9ec6eff7c67a2c7300712c
Austin Seipp <aseipp@pobox.com>
2022-12-20 03:35:32 -0600

    installer: don't 'direnv allow' implicitly with --skip-confirm

--- .github/actions/direnv/index.js
+++ .github/actions/direnv/index.js
@@ -3,6 +3,9 @@ const cp = require("child_process");
 
 async function run() {
     try {
+        // [tag:direnv-allow-ci] We always allow any directory; this helps us avoid
+        // cases where we want to make it 'easy' to run direnv on behalf of the user,
+        // like in the installer.
         cp.execSync('direnv allow', { encoding: "utf-8" });
         const envs = JSON.parse(cp.execSync('direnv export json', { encoding: "utf-8" }));
 
