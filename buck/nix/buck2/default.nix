c8170deb61f85ea77a5474eaa2ce2edfe579a5c1
Austin Seipp <aseipp@pobox.com>
2022-12-20 15:12:47 -0600

    chore(buck): patch out an annoying warning
    Summary: This comes from the Buck daemon on first build, and is rather ugly
    given how pretty and beautiful the output otherwise is. It just tells us we
    can't do remote execution or caching.
    
    But since functional caching and remote execution aren't even in the FOSS
    version of buck yet, it doesn't really matter, I think.
    
    Once buck2 is bootstrapped with buck2 (super fun), this can be dropped.
    
    Test Plan: `touch .envrc` to rebuild buck

--- buck/nix/buck2/default.nix
+++ buck/nix/buck2/default.nix
@@ -35,7 +35,17 @@ in rustPlatform.buildRustPackage rec {
   };
 
   doCheck = false;
-  postPatch = "cp ${./Cargo.lock} Cargo.lock";
+
+  # Put in the Cargo.lock file.
+  #
+  # XXX NOTE (aseipp): Also, for now, suppress a really annoying 'tracing'
+  # warning that makes the default build output uglier; once we self-bootstrap
+  # buck2 with buck2 under Nix (ugh...) then we can get rid of this.
+  postPatch = ''
+    cp ${./Cargo.lock} Cargo.lock
+    substituteInPlace buck2_server/src/daemon/common.rs \
+      --replace 'tracing::warn!("Cargo build detected:' '//tracing::warn!("Cargo build detected:'
+  '';
 
   postInstall = ''
     mv $out/bin/buck2     $out/bin/buck
